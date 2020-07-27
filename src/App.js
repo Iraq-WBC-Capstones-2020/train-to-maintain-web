4958552527942b1e75ad7198008b5aa1937f5636
evan hameed <evanhameed9966@gmail.com>
2020-07-27 19:21:26 +0300

    deleting h1 from app.js (boilerplate header)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import BlogCard from './components/Card/BlogCard';
 function App() {
   return (
     <div>
-      <h1 className="text-3xl text-purple-500">Train To Maintain</h1>
       <BlogCard />
     </div>
   );
