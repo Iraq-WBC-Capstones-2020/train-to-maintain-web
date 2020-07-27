79dd27652a1d4049e11918b9449d0fe1eae83273
evan hameed <evanhameed9966@gmail.com>
2020-07-27 22:55:14 +0300

    Adding some basic jsx with somecontent to BlogCard

--- src/components/Card/BlogCard.js
+++ src/components/Card/BlogCard.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const BlogCard = () => {
-  return <div> </div>;
+  return (
+    <React.Fragment>
+      <div>
+        <div></div>
+        <div>
+          <div>Can coffee make you a better developer?</div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
+            perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default BlogCard;
