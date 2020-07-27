061a46aa69a97761fe2b736e5d5a1a9f503198c6
evan hameed <evanhameed9966@gmail.com>
2020-07-27 19:19:59 +0300

    Importing BlogCard.js in App.js to see the results

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BlogCard from './components/Card/BlogCard';
 
 function App() {
   return (
     <div>
       <h1 className="text-3xl text-purple-500">Train To Maintain</h1>
+      <BlogCard />
     </div>
   );
 }
