994ffacb424db9b9e6002dfb074f331ea69eeadd
osamaakb <osama98akb@gmail.com>
2020-07-27 17:03:13 +0300

    Feat: add tailwind css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Train To Maintain</h1>
+      <h1 className="text-3xl text-purple-500">Train To Maintain</h1>
     </div>
   );
 }
