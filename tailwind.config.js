8d081083a8bd2afb4dc6601dbff755f828ab4a83
evan hameed <evanhameed9966@gmail.com>
2020-07-27 23:20:43 +0300

    adding the colors we used in figma to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     extend: {
       colors: {
         'purple-project': '#4527A0',
+        'offwhite-project': '#f2f2f2',
+        'pink-project': '#FF619A',
+        'darkgray-project ': '#272727',
       },
     },
   },
