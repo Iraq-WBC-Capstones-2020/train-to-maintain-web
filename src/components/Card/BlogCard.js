52e0c1d6cb877ca19ae41e6522fc6cc10e5cc554
evan hameed <evanhameed9966@gmail.com>
2020-07-28 03:41:31 +0300

    changing content of the website of  card's body

--- src/components/Card/BlogCard.js
+++ src/components/Card/BlogCard.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import { motion } from 'framer-motion';
 const BlogCard = () => {
   return (
     <React.Fragment>
-      <div className="max-w-sm w-full md:w-11/12 lg:w-10/12 md:max-w-full md:flex md:justify-center  my-10">
+      <motion.div
+        whileHover={{ scale: 1.07 }}
+        whileTap={{ scale: 0.8 }}
+        className="max-w-sm w-full md:w-11/12 lg:w-10/12 md:max-w-full md:flex md:justify-center  my-10"
+      >
         <div
-          className="h-56 md:h-auto md:w-48 flex-none bg-cover  text-center overflow-hidden shadow-xl"
+          className="h-56 md:h-auto md:w-64 flex-none bg-cover  text-center overflow-hidden shadow-xl"
           style={{
             backgroundImage: "url('https://tailwindcss.com/img/card-top.jpg')",
             backgroundPosition: 'center',
@@ -15,18 +19,17 @@ const BlogCard = () => {
         <div className=" bg-purple-project  p-4 flex flex-col justify-between leading-normal shadow-xl font-font-project">
           <div className="mb-8">
             <div className="text-gray-100 font-bold text-xl mb-2">
-              Can coffee make you a better developer?
+              10 tips I wish I knew before I started to workout.
             </div>
             <p className="text-gray-100 text-base md:w-9/12s">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Voluptatibus quia, nulla! Maiores et perferendis eaque,
-              exercitationem praesentium nihil. Lorem ipsum dolor sit amet,
-              consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
-              perferendis eaque, exercitationem praesentium nihil.
+              If you have been planning to start working out for the last couple
+              of months but you always procrastinate about how, where, and when
+              to start, here are 10 tips that really helped throughout my
+              journey and I wish I knew from the beginning.
             </p>
           </div>
         </div>
-      </div>
+      </motion.div>
     </React.Fragment>
   );
 };
