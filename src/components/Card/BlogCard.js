42b503ba30151b0dd3ecf22003097893ebeb5da8
evan hameed <evanhameed9966@gmail.com>
2020-07-28 04:04:39 +0300

    adding author to the card

--- src/components/Card/BlogCard.js
+++ src/components/Card/BlogCard.js
@@ -18,15 +18,16 @@ const BlogCard = () => {
         ></div>
         <div className=" bg-purple-project  p-4 flex flex-col justify-between leading-normal shadow-xl font-font-project">
           <div className="mb-8">
-            <div className="text-gray-100 font-bold text-xl mb-2">
+            <div className="text-gray-100 font-bold text-xl mb-2 ">
               10 tips I wish I knew before I started to workout.
             </div>
-            <p className="text-gray-100 text-base md:w-9/12s">
+            <p className="text-gray-100 text-base md:w-9/12s mb-2">
               If you have been planning to start working out for the last couple
               of months but you always procrastinate about how, where, and when
               to start, here are 10 tips that really helped throughout my
               journey and I wish I knew from the beginning.
             </p>
+            <p className="mt-12">By Evan Hameed</p>
           </div>
         </div>
       </motion.div>
