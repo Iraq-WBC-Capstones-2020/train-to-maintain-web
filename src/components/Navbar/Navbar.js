ea3b528fdd56c6ff2e5ddf2f4c99642394146e2d
evan hameed <evanhameed9966@gmail.com>
2020-07-30 09:45:55 +0300

    defining navbarOpen useState

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { motion } from 'framer-motion';
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
   return <div></div>;
 };
 
