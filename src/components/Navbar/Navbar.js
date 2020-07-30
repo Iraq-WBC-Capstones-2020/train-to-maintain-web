db97053b8fdfae26d2b1b4f4416b4286497662de
evan hameed <evanhameed9966@gmail.com>
2020-07-30 09:44:56 +0300

    importing framer motion

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { motion } from 'framer-motion';
 const Navbar = () => {
   return <div></div>;
 };
