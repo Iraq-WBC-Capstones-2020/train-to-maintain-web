a6a73fad83bb1b603aaac2d637b658113c7a77ba
evan hameed <evanhameed9966@gmail.com>
2020-07-30 09:51:24 +0300

    importing the Navbar componenet in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import BlogCard from './components/Card/BlogCard/BlogCard';
-
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div>
+      <Navbar />
       <BlogCard />
       <BlogCard />
       <BlogCard />
