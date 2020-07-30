35f1950277e70993f355a7cf237dd37420c1bc28
evan hameed <evanhameed9966@gmail.com>
2020-07-30 09:44:07 +0300

    importing font awesome icon (Hamburger)

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 const Navbar = () => {
   return <div></div>;
 };
