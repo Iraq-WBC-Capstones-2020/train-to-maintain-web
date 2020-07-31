a5bdc87479d78f8efa12a019e92b35ccc2150692
evan hameed <evanhameed9966@gmail.com>
2020-07-31 19:26:36 +0300

    Adding a side nav to the file

--- src/components/Navbar/SideNav.js
+++ src/components/Navbar/SideNav.js
@@ -1,7 +1,40 @@
 import React from 'react';
-
-const SideNav = () => {
-  return <div></div>;
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+const SideBar = (props) => {
+  let drawerClasses = 'drawer';
+  if (props.navbarOpen === true) {
+    drawerClasses = 'drawer open';
+  }
+  return (
+    <nav
+      className={
+        `bg-pinky h-screen w-3/4 md:w-2/4  fixed top-0 right-0 z-10 text-white lg:hidden ` +
+        drawerClasses
+      }
+    >
+      <button className="m-3" onClick={() => props.handleHamClick()}>
+        <FontAwesomeIcon icon={faTimes} />
+      </button>
+      <ul className="h-full flex flex-col justify-around items-center  ">
+        <li>
+          <a href="#">HOME</a>
+        </li>
+        <li>
+          <a href="#">WORKOUTS</a>
+        </li>
+        <li>
+          <a href="#">MEALS</a>
+        </li>
+        <li>
+          <a href="#">BUNDLES</a>
+        </li>
+        <li>
+          <a href="#">ABOUT</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
-export default SideNav;
+export default SideBar;
