18c62a7cc47f35df239386d0d035f5afcf94cf24
evan hameed <evanhameed9966@gmail.com>
2020-07-31 20:23:58 +0300

    Adding conditional render to add open class to nav

--- src/components/Navbar/SideNav.js
+++ src/components/Navbar/SideNav.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import './SideNav.css';
 const SideBar = (props) => {
+  let sideNavClasses = 'SideNav';
+  if (props.navbarOpen) {
+    sideNavClasses = 'SideNav open';
+  }
   return (
     <nav
-      className={`bg-darkgray h-screen w-3/4 md:w-2/4  fixed top-0 right-0 z-10 text-white lg:hidden `}
+      className={
+        `bg-darkgray h-screen w-3/4 md:w-2/4  fixed top-0 right-0 z-10 text-white lg:hidden ` +
+        sideNavClasses
+      }
     >
       <button className="m-3" onClick={() => props.handleHamClick()}>
         <FontAwesomeIcon icon={faTimes} />
