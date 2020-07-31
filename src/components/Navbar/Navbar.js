1066aabb36b9d910c0e6e6bf8b98a12e4b4ec7d9
evan hameed <evanhameed9966@gmail.com>
2020-07-31 19:26:23 +0300

    changing the names of the componenets

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Toolbar from './Toolbar';
 import SideNav from './SideNav';
+
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+  const handleHamClick = () => {
+    setNavbarOpen((prevState) => !prevState);
+  };
   return (
     <>
-      <Toolbar />
-      <SideNav />
+      <Toolbar navbarOpen={navbarOpen} handleHamClick={handleHamClick} />
+      {navbarOpen ? (
+        <SideNav handleHamClick={handleHamClick} navbarOpen={navbarOpen} />
+      ) : (
+        ''
+      )}
     </>
   );
 };
