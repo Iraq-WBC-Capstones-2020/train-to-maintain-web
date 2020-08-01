f6ec89dc762c7cb2a51c90fcc3b7df294e249098
evan hameed <evanhameed9966@gmail.com>
2020-08-01 17:06:05 +0300

    correcting a typo inside the app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
-import Navbar from './components/Navbar/Navbar';
+
 import BlogCard from './components/Card/BlogCard/BlogCard';
 test('render children succesfully', () => {
-  render(<Navbar />);
   render(<BlogCard />);
 });
