f720e4f30641387d921b473dbd199e8bc6cec5e2
evan hameed <evanhameed9966@gmail.com>
2020-08-01 21:58:44 +0300

    remove boilerplate code from app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import BlogCard from './components/Card/BlogCard/BlogCard';
 function App() {
   return (
     <div>
-      <BlogCard
-        title="10 tips I wish I knew before I started to workout."
-        imgUrl="https://tailwindcss.com/img/card-top.jpg"
-        overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning"
-        author="Evan Hameed"
-        publishDate="jul 19"
-      />
     </div>
   );
 }
