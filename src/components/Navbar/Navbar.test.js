790bb4d77b4f8e3058932237b91394ba8282f264
evan hameed <evanhameed9966@gmail.com>
2020-08-01 15:50:29 +0300

    moving comps that doesnt belong to blogcard branch

--- src/components/Navbar/Navbar.test.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import Navbar from './Navbar';
-test('Navbar renders succesfully', () => {
-  render(<Navbar />);
-});
