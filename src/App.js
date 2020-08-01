9e0b720a94ea89c3da89fa0bd8d3327719d3df71
evan hameed <evanhameed9966@gmail.com>
2020-08-01 22:33:33 +0300

    set a route to each container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './containers/LandingPage/LandingPage';
+import Home from './containers/Home/Home';
+import Meals from './containers/Meals/Meals';
+import Workouts from './containers/Workouts/Workouts';
+import About from './containers/About/About';
+import Blogs from './containers/Blogs/Blogs';
 function App() {
-  return <React.Fragment>
 
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={Home} />
+          <Route path="/meals" component={Meals} />
+          <Route path="/workouts" component={Workouts} />
+          <Route path="/about" component={About} />
+          <Route path="/blogs" component={Blogs} />
+        </Switch>
+      </Router>
+    </React.Fragment>
+  );
+
 }
 
 export default App;
