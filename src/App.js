3c191241e0d5b11fe2ce2a353684048cef4355ed
evan hameed <evanhameed9966@gmail.com>
2020-08-01 15:51:34 +0300

    cleaning app.js from removed components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import BlogCard from './components/Card/BlogCard/BlogCard';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+
 function App() {
   return (
     <div>
-      <Navbar />
       <BlogCard />
       <BlogCard />
       <BlogCard />
-      <Footer />
     </div>
   );
 }
