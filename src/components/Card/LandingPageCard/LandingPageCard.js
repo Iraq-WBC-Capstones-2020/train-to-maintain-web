6cbb1cecc2a24789ca9981e5bb9ebf3ed6505c02
Mahmoud <mahmodlte@gmail.com>
2020-08-02 16:26:26 +0300

    changed font size, padding around title and add fixed image size

--- src/components/Card/LandingPageCard/LandingPageCard.js
+++ src/components/Card/LandingPageCard/LandingPageCard.js
@@ -3,9 +3,13 @@ import propTypes from 'prop-types';
 const LandingPageCard = ({ image, title }) => {
   return (
     <>
-      <div className=" max-h-lg max-w-sm sm:w-1/2 lg:w-1/4  overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 rounded">
-        <img className="w-full" src={image} alt="Train to Maintain landing" />
-        <div className="font-bold text-xl text-purple-600 text-center p-10">
+      <div className=" max-h-lg max-w-sm sm:w-1/2 lg:w-1/4 overflow-hidden shadow-2xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 ">
+        <img
+          className="h-64 w-11/12 mx-auto object-cover"
+          src={image}
+          alt="Train to Maintain landing"
+        />
+        <div className="font-bold text-2xl text-purple-600 text-center p-5">
           {title}
         </div>
       </div>
