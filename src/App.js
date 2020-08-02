b2e96d54a6810924f44c0f8926e3826225ea68ab
Mahmoud <mahmodlte@gmail.com>
2020-08-02 08:15:42 +0300

    Landing page card done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import LandingPageCard from './components/Card/LandingPageCard';
 function App() {
   return (
     <div>
       <h1 className="text-3xl text-purple-500">Train To Maintain</h1>
+      <LandingPageCard
+        image={
+          'https://live.staticflickr.com/1922/30712761087_9657616fea_b.jpg'
+        }
+        title={'Meals'}
+      />
     </div>
   );
 }
---
+++ src/components/Card/LandingPageCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import propTypes from 'prop-types';
+const LandingPageCard = ({ image, title }) => {
+  return (
+    <>
+      <div className=" max-h-lg max-w-sm sm:w-1/2 lg:w-1/4  overflow-hidden shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 rounded">
+        <img className="w-full" src={image} alt="Train to Maintain landing" />
+        <div className="font-bold text-xl text-purple-600 text-center p-10">
+          {title}
+        </div>
+      </div>
+    </>
+  );
+};
+LandingPageCard.propTypes = {
+  title: propTypes.string.isRequired,
+  image: propTypes.string.isRequired,
+};
+export default LandingPageCard;
