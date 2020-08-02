53db0597b2db7b5e0c9508e6bb56352edb747968
evan hameed <evanhameed9966@gmail.com>
2020-08-02 13:24:07 +0300

    style notfound page

--- src/containers/NotFound/NotFound.js
+++ src/containers/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div className="h-screen text-purple-700 bg-secondary flex items-center justify-center">
+    <div className="h-screen text-primary bg-secondary flex items-center justify-center">
       <h1>Oh no!!! Page Not found</h1>
     </div>
   );
