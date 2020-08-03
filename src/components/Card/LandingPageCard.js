6e62d264a4cef3694502b5fcee756e50ae7aa7d3
Mahmoud <mahmodlte@gmail.com>
2020-08-03 08:07:40 +0300

    made the changes and used framer motion for hovering effect

--- src/components/Card/LandingPageCard.js
+++ src/components/Card/LandingPageCard.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { motion } from 'framer-motion';
+
 const LandingPageCard = ({ image, title }) => {
   return (
-    <>
-      <div className=" max-h-lg max-w-sm sm:w-1/2 lg:w-1/4 overflow-hidden shadow-2xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 ">
-        <img
-          className="h-64 w-11/12 mx-auto object-cover"
-          src={image}
-          alt="Train to Maintain landing"
-        />
-        <div className="font-bold text-2xl text-purple-600 text-center p-5">
-          {title}
-        </div>
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      className=" max-h-lg max-w-sm sm:w-1/2 lg:w-1/4 overflow-hidden shadow-2xl  "
+    >
+      <img
+        className="h-64 w-11/12 mx-auto object-cover"
+        src={image}
+        alt="Train to Maintain landing"
+      />
+      <div className="font-bold text-2xl text-purple-600 text-center p-5">
+        {title}
       </div>
-    </>
+    </motion.div>
   );
 };
 LandingPageCard.propTypes = {
