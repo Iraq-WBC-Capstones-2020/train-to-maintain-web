bd9317ba92f835d2ab7550ca23ba8de0af715e96
Ahmed-Alassaf <ahmedfaysal199@gmail.com>
2020-08-03 23:12:11 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import BlogCard from './components/Card/BlogCard/BlogCard';
+import MealType from './components/MealType/MealType';
+import img from './components/MealType/img/wtgain.png';
 function App() {
   return (
     <div>
-      <BlogCard
-        title="10 tips I wish I knew before I started to workout."
-        imgUrl="https://tailwindcss.com/img/card-top.jpg"
-        overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning"
-        author="Evan Hameed"
-        publishDate="jul 19"
-      />
+      <MealType id="1" title="Gain weight" imgUrl={img} />
     </div>
   );
 }
