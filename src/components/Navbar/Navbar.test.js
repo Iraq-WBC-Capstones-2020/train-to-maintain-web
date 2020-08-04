c58a61a2db5f078db59f73337888975f851d7e34
evan hameed <evanhameed9966@gmail.com>
2020-08-04 12:11:20 +0300

    fix:navbar.test.js

--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './Navbar';
 test('Navbar renders succesfully', () => {
-  render(<Navbar />);
+  <Router>
+    render(
+    <Navbar />
+    );
+  </Router>;
 });
