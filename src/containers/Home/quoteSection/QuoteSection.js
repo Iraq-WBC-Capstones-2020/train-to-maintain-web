8eed7a0dabf538c94eda30a3f18204045e036f83
evan hameed <evanhameed9966@gmail.com>
2020-08-05 20:57:28 +0300

    feat: add quote sec to home

---
+++ src/containers/Home/quoteSection/QuoteSection.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import './QuoteSection.css';
+const QuoteSection = () => {
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    fetchQuotes();
+  }, []);
+  const fetchQuotes = () => {
+    fetch('https://type.fit/api/quotes')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setQuote(data[Math.floor(Math.random() * data.length)].text);
+      });
+  };
+
+  return (
+    <section className="flex justify-center items-center lg:py-32 md:py-24 py-16 quote-section  w-screen">
+      <div className="  w-9/12 flex justify-center ">
+        <h1 className="font-bold text-secondary lg:text-4xl text-xl">
+          {quote}
+        </h1>
+      </div>
+    </section>
+  );
+};
+
+export default QuoteSection;
