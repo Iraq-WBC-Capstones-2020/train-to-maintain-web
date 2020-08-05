2c24df9a891c4b561f5721509c0db95cb9e01c38
evan hameed <evanhameed9966@gmail.com>
2020-08-05 15:19:50 +0300

    Feat: add header to home

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
-
+import './Home.css';
+import Header from './header/Header';
+import bgvideo from './assets/bgvideo.mp4';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <video
+        id="bg-video"
+        autoPlay
+        muted
+        loop
+        className="w-screen  absolute left-0 top-0 object-cover "
+      >
+        <source src={bgvideo} type="video/mp4" />
+      </video>
+      <Header />
+      <div className="mt-4">that is another div</div>
+    </>
+  );
 };
 
 export default Home;
