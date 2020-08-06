ee137f09c55d6a65cf5eed9ae53cf7fdc29db09d
Mahmoud <mahmodlte@gmail.com>
2020-08-06 08:52:11 +0300

    Footer added Link for react router

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import workoutImage from './assets/workoutImage.jpg';
+import { Link } from 'react-router-dom';
 
-// style={{
-//   backgroundImage: `url(${workoutImage})`,
-//   backgroundSize: 'cover',
-//   backgroundPosition: 'center',
-// }}
 const Footer = () => {
   return (
     <div className=" sm:h-auto lg:h-64 flex lg:flex-row xl:flex-row flex-col md:flex-row justify-between items-center font-poppins text-secondary">
@@ -17,21 +13,22 @@ const Footer = () => {
         Train to{' '}
         <span className="border-b-4 border-primary z-40">Maintain</span>
       </h1>
+
       <ul className="text-2xl sm:p-2 p-2 relative">
         <li>
-          <a href="#">Home</a>
+          <Link to="/home">Home</Link>
         </li>
         <li>
-          <a href="#">Workout</a>
+          <Link to="/workouts">Workouts</Link>
         </li>
         <li>
-          <a href="#">Meals</a>
+          <Link to="/meals">Meals</Link>
         </li>
         <li>
-          <a href="#">Bundles</a>
+          <Link to="/about">About</Link>
         </li>
-        <li>About</li>
       </ul>
+
       <div className="p-5 w-56 relative">
         <button className="bg-primary w-20 h-10 text-secondary mr-2">
           Sign Up
