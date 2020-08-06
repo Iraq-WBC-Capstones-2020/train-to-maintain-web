e3d7f5d46d99003e5d697012471ac68372aa9d14
Ahmed-Alassaf <ahmedfaysal199@gmail.com>
2020-08-06 20:40:23 +0300

    Added CSS file

--- src/components/MealType/MealType.js
+++ src/components/MealType/MealType.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import './MealType.css';
 import propTypes from 'prop-types';
 
 const MealType = ({ title, imgUrl }) => {
   return (
     <div
-      className="h-64 w-screen md:h-screen md:w-1/2 flex items-center justify-center"
+      className="mealImage"
       style={{
         backgroundImage: `url(${imgUrl})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
       }}
     >
-      <button className="bg-black bg-opacity-75 hover:bg-primary hover:bg-opacity-75 px-12 py-6 sm:px-16 sm:py-9 xl:px-20 xl:py-12">
-        <h1 className="font-bold font-poppins text-secondary text-3xl sm:text-5xl xl:text-6xl">
-          {title}
-        </h1>
+      <button className="box">
+        <h1 className="text">{title}</h1>
       </button>
     </div>
   );
