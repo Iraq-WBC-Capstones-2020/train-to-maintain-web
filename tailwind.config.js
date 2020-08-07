ee414e2ce0c0165bdf63eb32e276ebb4f8a6a3fc
Shirin Muataz <shirinmuataz@gmail.com>
2020-08-08 00:17:07 +0300

    solving meging conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,6 @@
-
 module.exports = {
   purge: [],
   theme: {
-<<<<<<< HEAD
-    extend: {
-      colors: {
-        primary: '#4527A0',
-        secondary: '#f2f2f2',
-        pinky: '#FF619A',
-        darkgray: '#272727',
-      },
-      fontFamily: {
-        poppins: ['Poppins'],
-      },
-    },
-||||||| c01adfc
-    extend: {},
-=======
     extend: {
       colors: {
         primary: '#4527A0',
@@ -28,17 +12,10 @@ module.exports = {
         poppins: ['Poppins'],
       },
       height: {
-        'screen-50': "50vh"
+        'screen-50': '50vh',
       },
     },
->>>>>>> e9df546dc7e24355b7c3135537d6ac79371aa22c
   },
   variants: {},
   plugins: [],
-<<<<<<< HEAD
 };
-||||||| c01adfc
-}
-=======
-};
->>>>>>> e9df546dc7e24355b7c3135537d6ac79371aa22c
