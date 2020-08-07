4a5f0bce376e40c44c7d0526679fbe66fc1d9fdf
osamaakb <osama98akb@gmail.com>
2020-08-07 12:51:18 +0300

    Style: remove unneeded css classes

--- src/components/MealType/MealType.js
+++ src/components/MealType/MealType.js
@@ -5,15 +5,15 @@ import propTypes from 'prop-types';
 const MealType = ({ title, imgUrl }) => {
   return (
     <div
-      className="mealImage"
+      className="mealImage flex justify-center items-center h-screen w-screen"
       style={{
         backgroundImage: `url(${imgUrl})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
       }}
     >
-      <button className="box">
-        <h1 className="text">{title}</h1>
+      <button className="bg-white opacity-50 hover:text-white hover:bg-primary p-5">
+        <h1 className="lg:text-6xl text-5xl font-bold">{title}</h1>
       </button>
     </div>
   );
