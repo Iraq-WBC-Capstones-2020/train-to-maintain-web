2081a26b4a271ad34f779e7c914b9f116b455d37
osamaakb <osama98akb@gmail.com>
2020-08-07 16:28:34 +0300

    Style(lint): RecipeCard

--- src/components/Card/RecipeCard/RecipeCard.js
+++ src/components/Card/RecipeCard/RecipeCard.js
@@ -15,8 +15,13 @@ export default function RecipeCard({
   return (
     <motion.div
       whileHover={{ scale: 1.02 }}
-      className="lg:w-1/4 xl:w-1/3 md:w-1/3 sm:w-full overflow-hidden shadow-lg m-4">
-      <img className="h-48 w-full mx-auto object-cover" src={image} alt="breakfast" />
+      className="lg:w-1/4 xl:w-1/3 md:w-1/3 sm:w-full overflow-hidden shadow-lg m-4"
+    >
+      <img
+        className="h-48 w-full mx-auto object-cover"
+        src={image}
+        alt="breakfast"
+      />
       <div className="px-4 py-2 flex flex-col justify-around items-center">
         <h1 className="text-black-700 font-bold text-xl m-2 text-center">
           {title}
