ca2d609b8c45f9b27eeadb13a64a5d6a4d89fa6b
evan hameed <evanhameed9966@gmail.com>
2020-08-08 13:05:34 +0300

    reduce sizes of some files

--- src/containers/Home/soical media/SocialMedia.js
+++ src/containers/Home/soical media/SocialMedia.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './SocialMedia.css';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 import { motion } from 'framer-motion';
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion';
 const SocialMedia = () => {
   return (
     <section className="social-media  lg:h-xxl md:h-xxl flex flex-col items-center py-8 ">
-      <h1 className="md:text-3xl text-lg text-center font-bold text-darkgray lg:mb-8 w-10/12">
+      <h1 className="md:text-3xl text-lg text-center font-bold text-secondary lg:mb-8 w-10/12">
         Get the most out of socail media content creators
       </h1>
       <div className="flex  items-center w-screen   overflow-x-scroll disable-scrollbar lg:mb-8">
