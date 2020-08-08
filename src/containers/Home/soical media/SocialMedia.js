d5a94b4de662346389380f40bbb68c0afbb5356e
evan hameed <evanhameed9966@gmail.com>
2020-08-08 13:05:34 +0300

    reduce sizes of some files

--- src/containers/Home/soical media/SocialMedia.js
+++ src/containers/Home/soical media/SocialMedia.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
 import './SocialMedia.css';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
 import { motion } from 'framer-motion';
