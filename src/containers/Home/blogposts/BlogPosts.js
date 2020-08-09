0c71da883a43ad9a5eb2fd1e7055d2d37c2cf9ae
evan hameed <evanhameed9966@gmail.com>
2020-08-09 14:26:09 +0300

    refactor home

--- src/containers/Home/blogposts/BlogPosts.js
+++ src/containers/Home/blogposts/BlogPosts.js
@@ -14,17 +14,22 @@ const BlogPosts = () => {
         <BlogCard
           title="10 tips I wish I knew before I started to workout."
           overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning."
-          imgUrl="https://www.google.com/search?q=placeholder+image&tbm=isch&ved=2ahUKEwiv0LXCmIbrAhUN4RoKHXyaAhAQ2-cCegQIABAA&oq=placeholder+image&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BAgjECc6BAgAEEM6BQgAELEDOgcIIxDqAhAnOgcIABCxAxBDUKeDBViEpwVg_qcFaAJwAHgBgAH4AYgBphOSAQYwLjE2LjKYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=h8MrX-_wKo3Ca_y0ioAB&bih=789&biw=1440&rlz=1C5CHFA_enIQ896IQ896#imgrc=fFTEG8vPy3K58M"
+          imgUrl="https://permobil.com.au/wp-content/uploads/2017/08/no-image-placeholder-2.jpg"
         />
         <BlogCard
           title="10 tips I wish I knew before I started to workout."
           overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning."
-          imgUrl="https://www.google.com/search?q=placeholder+image&tbm=isch&ved=2ahUKEwiv0LXCmIbrAhUN4RoKHXyaAhAQ2-cCegQIABAA&oq=placeholder+image&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BAgjECc6BAgAEEM6BQgAELEDOgcIIxDqAhAnOgcIABCxAxBDUKeDBViEpwVg_qcFaAJwAHgBgAH4AYgBphOSAQYwLjE2LjKYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=h8MrX-_wKo3Ca_y0ioAB&bih=789&biw=1440&rlz=1C5CHFA_enIQ896IQ896#imgrc=fFTEG8vPy3K58M"
+          imgUrl="https://permobil.com.au/wp-content/uploads/2017/08/no-image-placeholder-2.jpg"
         />
         <BlogCard
           title="10 tips I wish I knew before I started to workout."
           overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning."
-          imgUrl="https://www.google.com/search?q=placeholder+image&tbm=isch&ved=2ahUKEwiv0LXCmIbrAhUN4RoKHXyaAhAQ2-cCegQIABAA&oq=placeholder+image&gs_lcp=CgNpbWcQAzICCAAyAggAMgIIADICCAAyAggAMgIIADICCAAyAggAMgIIADICCAA6BAgjECc6BAgAEEM6BQgAELEDOgcIIxDqAhAnOgcIABCxAxBDUKeDBViEpwVg_qcFaAJwAHgBgAH4AYgBphOSAQYwLjE2LjKYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=h8MrX-_wKo3Ca_y0ioAB&bih=789&biw=1440&rlz=1C5CHFA_enIQ896IQ896#imgrc=fFTEG8vPy3K58M"
+          imgUrl="https://permobil.com.au/wp-content/uploads/2017/08/no-image-placeholder-2.jpg"
+        />
+        <BlogCard
+          title="10 tips I wish I knew before I started to workout."
+          overview="If you have been planning to start working out for the last couple of months but you always procrastinate about how, where, and when to start, here are 10 tips that really helped throughout my journey and I wish I knew from the beginning."
+          imgUrl="https://permobil.com.au/wp-content/uploads/2017/08/no-image-placeholder-2.jpg"
         />
       </div>
       <Link to="/blogs">
