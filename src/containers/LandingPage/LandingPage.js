1654f6cbc845fdb210693c8e3bb2465ffc6772b0
osamaakb <osama98akb@gmail.com>
2020-08-09 10:42:31 +0300

    Style: remove unneeded css classes
    removed landing.css file and fixed white bg issue

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -5,8 +5,6 @@ import { Link } from 'react-router-dom';
 import MealsPic from './assets/Meals.jpg';
 import WorkoutPic from './assets/Workout.jpg';
 import BundlesPic from './assets/Bundles.jpg';
-import BlackRectangle from './assets/Rectangle.png';
-import './landing.css';
 
 const LandingPage = () => {
   return (
@@ -15,31 +13,29 @@ const LandingPage = () => {
         autoPlay="autoplay"
         loop="loop"
         muted
-        className="w-full h-full top-0 left-0 object-cover fixed  black-effect"
+        className="w-full h-full object-cover fixed"
       >
         <source src={landingWorkout} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <img
-        src={BlackRectangle}
-        className="fixed top-0 left-0 h-screen w-full"
-      />
+      <div className="absolute w-screen h-screen w-full bg-black opacity-50">
+      </div>
       <div>
-        <h1 className=" select-none text-secondary text-center font-bold text-6xl relative">
+        <h1 className="text-secondary text-center font-bold text-6xl relative">
           Train To <span className="border-b-4 border-primary">Maintain</span>
         </h1>
-        <h2 className=" select-none text-secondary text-center font-bold text-2xl relative">
+        <h2 className="text-secondary text-center font-bold text-2xl relative">
           YOUR PERSONAL TRAINING ASSISTANT
         </h2>
-        <div className=" flex flex-col items-center md:flex-row md:h-xxxl lg:h-la  justify-evenly p-2 ">
-          <Link to="./meals">
+        <div className="flex flex-col items-center md:flex-row md:h-xxxl lg:h-la  justify-evenly p-2 ">
+          <Link to="/meals">
             <LandingPageCard image={MealsPic} title={'Meals'} />
           </Link>
-          <Link to="./workouts">
+          <Link to="/workouts">
             <LandingPageCard image={WorkoutPic} title={'Workout'} />
           </Link>
-          <Link to="./bundles">
-            <LandingPageCard image={BundlesPic} title={'Bundles'} />
+          <Link to="/home">
+            <LandingPageCard image={BundlesPic} title={'Home'} />
           </Link>
         </div>
       </div>
@@ -48,4 +44,3 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
-// md:flex-col md:h-xxxl lg:h-xl  justify-evenly p-2
