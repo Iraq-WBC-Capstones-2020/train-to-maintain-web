2e489a3cb782127c3d0736be0bdefac228ab347d
evan hameed <evanhameed9966@gmail.com>
2020-08-11 19:49:45 +0300

    create semantic jsx

--- src/containers/SingleWorkout/SingleWorkout.js
+++ src/containers/SingleWorkout/SingleWorkout.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const SingleWorkout = () => {
-  return <div>this is the single workout page</div>;
+  return (
+    <div className="my-16 md:pl-16  h-xl  p-8 md:flex ">
+      <div className="mb-4 md:w-7/12  md:pt-8">
+        <h1 className=" md:text-2xl text-xl font-bold text-primary">
+          CHEST WORKOUT
+        </h1>
+        <p className="text-darkgray">
+          This is an effective chest workout that targets all of the chest pecs.
+          (upper,middle, and lower chest)
+        </p>
+      </div>
+      <div className="md:w-5/12 w-full flex md:justify-center">
+        <img
+          className="md:w-7/12 w-full h-full"
+          src="https://i2.wp.com/behungrystayhumble.com/wp-content/uploads/2018/05/brutal-caucasian-handsome-fitness-men-on-diet-training-chest-pum-95191475.jpg?w=2123&ssl=1"
+        ></img>
+      </div>
+    </div>
+  );
 };
 
 export default SingleWorkout;
