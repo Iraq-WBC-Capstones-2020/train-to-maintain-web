3c35c7bbc5ab665cfb7e2b0994a0a37188facf82
Shirin Muataz <shirinmuataz@gmail.com>
2020-08-12 02:55:30 +0300

    setting basic look

--- src/components/Cards/Recipe-Card/Recipe-Card.js
+++ src/components/Cards/Recipe-Card/Recipe-Card.js
@@ -3,19 +3,13 @@ import { caloriesIcon } from './img/calories-icon.png';
 import { categoryIcon } from './img/meal-icon.png';
 import { durationIcon } from './img/clock.svg';
 
-export default function RecipeCard({
-  image,
-  title,
-  calories,
-  category,
-  duration,
-}) {
+export default function RecipeCard(props) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="w-auto h-65" src={image} alt="breakfast" />
+      <img className="w-auto h-65" src={props.image} alt="meal image" />
       <div className="px-6 py-4 flex flex-col justify-around items-center ">
         <h1 className="text-black-700 text-base font-bold text-xl mb-2 text-center">
-          {title}
+          {props.title}
         </h1>
         <button
           className="bg-primary text-white active:bg-pink-600 text-sm px-6 py-3 shadow hover:shadow-lg outline-none focus:outline-none mx-4"
@@ -27,31 +21,34 @@ export default function RecipeCard({
 
         <div className="my-4 flex flex-wrap justify-around items-center">
           <div className=" flex flex-col justify-center items-center w-1/3">
-            <img src={caloriesIcon} className="w-1/4 py-2" />
+            <img
+              src={require('./img/calories-icon.png')}
+              className="w-1/4 py-2"
+            />
             <span className="bg-gray-200 rounded-full text-sm text-black text-center">
-              {calories}
+              {props.calories}
             </span>
           </div>
 
           <div className="flex flex-col justify-center items-center w-1/3">
             <img
-              src={categoryIcon}
+              src={require('./img/meal-icon.png')}
               alt="Meal category"
               className="w-1/4 py-2"
             ></img>
             <span className=" bg-gray-200 rounded-full  text-sm text-black ">
-              {category}
+              {props.category}
             </span>
           </div>
 
           <div className=" flex flex-col justify-center items-center w-1/3">
             <img
-              src={durationIcon}
+              src={require('./img/clock.svg')}
               alt="Meal Preparation duration"
               className="w-1/4 py-2"
             ></img>
             <span className="bg-gray-200 rounded-full  text-sm text-black ">
-              {duration}
+              {props.duration}
             </span>
           </div>
         </div>
--- src/components/MealsPage/MealsPage.js
+++ src/components/MealsPage/MealsPage.js
@@ -1,86 +1,89 @@
 import React from 'react';
 import RecipeCard from '../Cards/Recipe-Card/Recipe-Card';
+//import breakfast1 from '../Cards/Recipe-Card/img/meals/breakfast/breakfast1.jpg';
 
 const mealList = [
   {
-    image: './img/breakfast/breakfast1.png',
+    image: './img/meals/breakfast/breakfast1.jpg',
     title: 'Kiwi, Greek yogurt & blueberries',
     calories: '95 Calories',
     category: 'Breakfast',
     duration: '2 min.',
   },
   {
-    image: './img/breakfast/breakfast2.png',
+    image:
+      'https://images.everydayhealth.com/images/creative-ways-to-enjoy-eggs-08-joyful-healthy-eats-406x406.jpg',
     title: 'Quick And Easy Two Minute Omelet',
     calories: '187 Calories',
     category: 'Breakfast',
     duration: '2 min.',
   },
   {
-    image: './img/breakfast/breakfast3.png',
+    image:
+      'https://i.pinimg.com/236x/57/57/5a/57575a79673cc0f0a0848bb5f328b588.jpg',
     title: 'Kiwi, Greek yogurt & blueberries',
     calories: '170 Calories',
     category: 'Breakfast',
     duration: '15 min.',
   },
   {
-    image: './img/lunch/lunch1.jpg',
+    image: './img/meals/lunch/lunch1.jpg',
     title: 'Classic Chicken Casserole',
     calories: '390 Calories',
     category: 'Lunch',
     duration: '30 min.',
   },
   {
-    image: './img/lunch/lunch2.jpg',
+    image: './img/meals/lunch/lunch2.jpg',
     title: 'Tuna Salad',
     calories: '187 Calories',
     category: 'Lunch',
     duration: '10 min.',
   },
   {
-    image: './img/lunch/lunch3.jpg',
+    image: './img/meals/lunch/lunch3.jpg',
     title: 'Turkey & Cheese Bites',
     calories: '150 Calories',
     category: 'Lunch',
     duration: '20-25 min.',
   },
   {
-    image: './img/dinner/dinner1.jpg',
+    image: './img/meals/dinner/dinner1.jpg',
     title: 'Mexican Stuffed Chicken with red rice',
     calories: '467 Calories',
     category: 'Dinner',
     duration: '25 min.',
   },
   {
-    image: './img/dinner/dinner2.jpg',
+    image: './img/meals/dinner/dinner2.jpg',
     title: 'Miso-roasted aubergine steaks with sweet potato',
     calories: '344 Calories',
     category: 'Dinner',
     duration: '20 min.',
   },
   {
-    image: './img/dinner/dinner3.jpg',
+    image: './img/meals/dinner/dinner3.jpg',
     title: 'Super smoky bacon & tomato spaghetti',
     calories: '500 Calories',
     category: 'Dinner',
     duration: '25 min.',
   },
   {
-    image: './img/snacks/snack1.png',
+    image: './img/meals/snacks/snack1.png',
     title: ' Sweet Potato Chips',
     calories: '100 Calories',
     category: 'Snacks',
     duration: '2 hrs.',
   },
   {
-    image: './img/snacks/snack2.png',
+    image: './img/meals/snacks/snack2.png',
     title: '  Peanut Butter and Honey Oat Bar',
     calories: '157 Calories',
     category: 'Snacks',
     duration: '25 min.',
   },
   {
-    image: './img/snacks/snack3.png',
+    image: './img/meals/snacks/snack3.png',
     title: ' Dark Chocolate Nut Clusters',
     calories: '106 Calories',
     category: 'Snacks',
@@ -89,5 +92,12 @@ const mealList = [
 ];
 export default function MealsPage() {
   return (
-   
+    <div className="flex flex-wrap -mb-4">
+      {mealList.map((meal) => (
+        <div class="text-gray-700 text-center px-4 py-2 m-2">
+          <RecipeCard prop={meal} />
+        </div>
+      ))}
+    </div>
+  );
 }
