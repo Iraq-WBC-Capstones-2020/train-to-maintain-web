f184c3df0a4e94d652218bc067d139409499e565
Shirin Muataz <shirinmuataz@gmail.com>
2020-08-14 17:08:33 +0300

    fixed conflict

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: [],
   theme: {
-<<<<<<< HEAD
     extend: {
       height: {
         lg: '20rem',
@@ -9,38 +8,17 @@ module.exports = {
         xxl: '40rem',
         xxxl: '50rem',
         xxxxl: '60rem',
-      },
-||||||| merged common ancestors
-<<<<<<<<< Temporary merge branch 1
-    extend: {
-      colors: {
-        primary: '#4527A0',
-        secondary: '#f2f2f2',
-        pinky: '#FF619A',
-        darkgray: '#272727',
-      },
-      fontFamily: {
-        poppins: ['Poppins'],
-      },
-    },
-||||||||| c01adfc
-    extend: {},
-=========
-    extend: {
-=======
-    extend: {
->>>>>>> 2081a26b4a271ad34f779e7c914b9f116b455d37
-      colors: {
-        primary: '#4527A0',
-        secondary: '#f2f2f2',
-        pinky: '#FF619A',
-        darkgray: '#272727',
-      },
-      fontFamily: {
-        poppins: ['Poppins'],
-      },
-      height: {
         'screen-50': '50vh',
+      },
+
+      colors: {
+        primary: '#4527A0',
+        secondary: '#f2f2f2',
+        pinky: '#FF619A',
+        darkgray: '#272727',
+      },
+      fontFamily: {
+        poppins: ['Poppins'],
       },
     },
   },
